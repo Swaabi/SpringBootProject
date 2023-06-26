48f5ce42073813ebeccb73f13c5ea97c4967d088
Swaabi <priyaswathi672@gmail.com>
2023-06-26 11:20:26 +0530

    first commit

---
+++ SpringbootA/src/main/java/com/pack/SpringbootA/SpringbootAApplication.java
@@ -0,0 +1,13 @@
+package com.pack.SpringbootA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAApplication.class, args);
+	}
+
+}
---
+++ SpringbootA/src/test/java/com/pack/SpringbootA/SpringbootAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.SpringbootA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
